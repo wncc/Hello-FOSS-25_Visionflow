743a174cc1233d09d82e946fbe4fad805caef4fb
aashnapulla <aashnapulla@gmail.com>
2025-09-30 21:09:58 +0530

    Update __init__.py

--- visionflow/__init__.py
+++ visionflow/__init__.py
@@ -1,4 +1,2 @@
-from .test import test_func
-from .preprocess import preprocess_image
-
-__all__ = ["preprocess_image", "test_func"]
+from .pipeline import ClassificationPipeline
+from .preprocessing import Preprocessor
